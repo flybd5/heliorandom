65fcf1f887eed237385aae01df46941880fc27f4
Juan Jimenez <jjimenez.ctr@kr.af.mil>
2020-06-14 18:45:53 -0400

    Added MIT license

--- heliorandom.py
+++ heliorandom.py
@@ -1,8 +1,25 @@
+# Author: Juan Jiménez, flybd5@gmail.com
+# Copyright 2020 by Juan Jiménez
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 # Sample Python code to illustrate seeding Python's random number generator
-# with the datasum of the latest image of the sun from sources 8-17 on the
+# with the datasum of the latest image of the sun from one of sources 8-17 on the
 # Helioviewer API (SDO AIA instrument sources).
 
-# importing the required modules 
 import requests, json, random
 import xml.etree.ElementTree as ET
 from datetime import datetime
@@ -19,7 +36,7 @@ datetimestring = dtnow.strftime("%Y-%m-%dT%H:%M:%SZ")
 resp = requests.get('https://api.helioviewer.org/v2/getClosestImage/?date='+datetimestring+'&sourceId='+str(theSource))
 if resp.status_code != 200:
     print("Unable to connect to Helioviewer API.")
-else: 
+else:
     # parse the returned json and get the id
     resp_dict = json.loads(resp.text)
     id = resp_dict["id"]
@@ -31,9 +48,9 @@ else:
         resp = requests.get('https://api.helioviewer.org/v2/getJP2Header/?id='+str(id))
         if resp.status_code != 200:
             print("Unable to get FITS JSON data for image id:"+id+" from Helioviewer API.")
-        else:           
-            # create element tree object 
-            tree = ET.fromstring(resp.text) 
+        else:
+            # create element tree object
+            tree = ET.fromstring(resp.text)
             # go find the datasum
             datasum = tree.findtext('.//DATASUM')
             if datasum is None:
